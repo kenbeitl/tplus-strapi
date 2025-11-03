3d04e64239baba4ce0af18cee305c4cf5020bdee
Ken Bei <KenBei@tl.com>
2025-11-03 17:02:14 +0800

    basic setup for tplus form

---
+++ types/generated/components.d.ts
@@ -0,0 +1,40 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface FormsFormInfo extends Struct.ComponentSchema {
+  collectionName: 'components_forms_form_infos';
+  info: {
+    displayName: 'FormInfo';
+    icon: 'stack';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    formID: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.Unique;
+    formTitle: Schema.Attribute.String &
+      Schema.Attribute.DefaultTo<'Application Form'>;
+    submitButtonText: Schema.Attribute.String &
+      Schema.Attribute.DefaultTo<'Submit'>;
+  };
+}
+
+export interface SettingsService extends Struct.ComponentSchema {
+  collectionName: 'components_settings_services';
+  info: {
+    displayName: 'ServiceBase';
+    icon: 'cog';
+  };
+  attributes: {
+    icon: Schema.Attribute.Media<'images'>;
+    serviceName: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'forms.form-info': FormsFormInfo;
+      'settings.service': SettingsService;
+    }
+  }
+}
